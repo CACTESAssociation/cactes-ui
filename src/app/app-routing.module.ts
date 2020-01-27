d6a33e8e45c643007951e66dca0b7956f59fc807
Edwin Zhao <edwin.zhao@outlook.com>
2020-01-27 01:34:57 -0500

    Put down most of the fundamentals, need to add in the details within each page now.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,25 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomeContentComponent } from "./home-content/home-content.component";
+import { DonateComponent } from "./donate/donate.component";
+import { AboutComponent } from "./about/about.component";
+import { ContactComponent } from "./contact/contact.component";
+import { InvolvementComponent } from "./involvement/involvement.component";
+import { ProjectsComponent } from "./projects/projects.component";
+import { TeamComponent } from "./team/team.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomeContentComponent, pathMatch: "full" },
+  { path: "about", component: AboutComponent },
+  { path: "donate", component: DonateComponent },
+  { path: "contact", component: ContactComponent },
+  { path: "getinvolved", component: InvolvementComponent },
+  { path: "projects", component: ProjectsComponent },
+  { path: "team", component: TeamComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { NavbarComponent } from "./navbar/navbar.component";
+import { SidebarComponent } from "./sidebar/sidebar.component";
+import { HomeContentComponent } from "./home-content/home-content.component";
+import { AboutComponent } from "./about/about.component";
+import { ProjectsComponent } from "./projects/projects.component";
+import { TeamComponent } from "./team/team.component";
+import { ContactComponent } from "./contact/contact.component";
+import { InvolvementComponent } from "./involvement/involvement.component";
+import { DonateComponent } from "./donate/donate.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    HomeContentComponent,
+    AboutComponent,
+    ProjectsComponent,
+    TeamComponent,
+    ContactComponent,
+    InvolvementComponent,
+    DonateComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
