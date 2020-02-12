2131891313c23e6b2e00c1adbd1d82332ecbf3f6
Edwin Zhao <edwin.zhao@outlook.com>
2020-02-11 22:50:24 -0500

    Added bits of contact form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -26,8 +27,8 @@ import { DonateComponent } from "./donate/donate.component";
     InvolvementComponent,
     DonateComponent
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+  firstName: string;
+  lastName: string;
+  subject: string;
+  message: string;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  processForm() {
+    const email = `First name: ${this.firstName}\nLast name: ${this.lastName}` // Send to backend
   }
 
 }
